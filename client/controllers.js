fc7dae011c0fccfc8b52830c2315be04aba8f844
Sekhar Gogineni <sekhar.g@gmail.com>
2016-05-23 16:50:14 -0700

    test

--- client/controllers.js
+++ client/controllers.js
@@ -65,7 +65,8 @@ angular.module('storeApp').controller('storeController', ['$scope', '$routeParam
          //});*/
         //$scope.storerefresh = refreshedDataService.getData()
         $scope.method = 'GET';
-        $scope.url = 'https://api.myjson.com/bins/2nlhs';
+        //$scope.url = 'https://api.myjson.com/bins/2nlhs'; //fruits
+        $scope.url = 'https://api.myjson.com/bins/1l0u4';   //DTV products
 
         $http({method: $scope.method, url: $scope.url, cache: $templateCache})
             .then(function (response) {
